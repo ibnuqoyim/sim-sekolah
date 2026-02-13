4f1f897c5c3b31d52c7f75756045f6090425ef89
Muhamad Ibnu Qoyim <ibnuq@Muhamads-MacBook-Pro.local>
2026-02-13 13:18:44 +0700

    [fix] add env config

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useAuthStore = defineStore('auth', {
     state: () => ({
